49ea88af936a9a897ca904b8fc399199192f33ff
tmngoncalves <taniamngoncalves@gmail.com>
2021-10-10 21:43:31 -0300

    These are png files and the R files necessary to obtain them. In order to make each file self-contained, the code for the tidy data set was incorporated in every single file.

---
+++ plot4.R
@@ -0,0 +1,54 @@
+## The following code loads the data into a data frame and substitutes all "?" 
+## by NA.
+data <- read.table("household_power_consumption.txt",sep=";", 
+                   stringsAsFactors = FALSE,na.strings=c("?"))
+## With the following code, it is possible to see the first row of the data 
+## frame consists of the names of the variables. So the first row was used to
+## name the variables and then the first row of the data frame was removed.
+head(data)
+names(data) <- data[1,]
+names(data)
+dim(data)
+dataf <- data[2:2075260,]
+## The following code removes the rows with NA's.
+dataNAfree <- dataf[complete.cases(dataf),]
+## With this code, a new variable is created: Date-Time. This new variable is 
+## necessary to create the plots: it is the variable associated to the x axis. 
+## Furthermore, the data for this new variable are transformed into the type
+## Date/Time, in order to allow plotting. Finally, the data is filtered in order 
+## to keep only two dates: 1st and 2nd of february 2007.
+df <- dataNAfree %>% mutate(Date_Time=paste(Date, Time, sep = " ")) %>% 
+        mutate(Date_Time=strptime(Date_Time,format="%d/%m/%Y %H:%M:%S")) %>% 
+        filter(Date == "1/2/2007" | Date == "2/2/2007")
+## All the data that is not related to date and time, are transformed to the 
+## numeric type. This is necessary in order to be able to plot.
+df[,c(3:9)] <- sapply(df[,c(3:9)],as.numeric)
+
+## This code create a png file named plot4.png.
+png(filename="plot4.png",width=480,height=480,units="px")
+## This code allows to have 2 plots per row and column.
+par(mfrow=c(2,2),mar=c(4,4,2,2))
+## This code generates the plot which shows the change of global active power 
+## through time.
+with(df,plot(Date_Time,Global_active_power,type="l",xlab="",
+                                 ylab="Global Active Power"))
+## This code generates the plot which shows the change of voltage
+## through time. Note that the type "l" in this plot means it is going to be a 
+## line (piecewise curve, where each piece is a line).
+with(df,plot(Date_Time,Voltage,type="l",xlab="datetime",ylab="Voltage"))
+## This code generates the plot which shows the change of energy submetering 
+## through time for certain areas in a household: kitchen, laundry room, and 
+## water-heater and air-conditioning.
+with(df,plot(Date_Time,Sub_metering_1,type="l",col="black",xlab="",
+                                        ylab="Energy sub metering"))
+with(df,lines(Date_Time,Sub_metering_2,col="red"))
+with(df,lines(Date_Time,Sub_metering_3,col="blue"))
+## Note here the bty removes the legend box and cex decreases its default size.
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+                     col=c("black","red","blue"),lty=c(1,1,1),bty="n",cex=0.75)
+## This code generates a plot which shows the change o global reactive power 
+## through time.
+with(df,plot(Date_Time,Global_reactive_power,type="l",xlab="datetime"))
+## This code closes the file device. If this code is not executed, it is 
+## impossible to visualize the png file.
+dev.off()
